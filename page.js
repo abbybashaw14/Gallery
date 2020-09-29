a43f3d556d095e8bc01766f2a3a62439387378cb
abbybashaw14 <70040127+abbybashaw14@users.noreply.github.com>
2020-09-29 16:35:48 -0400

    added js

--- page.js
+++ page.js
@@ -6,3 +6,42 @@ function navToggle () {
     x.className = "topnav";
   }
 }
+
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides (n) {
+  showSlides (slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides (n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var indicatior = documetn.getElementsByClassName("indicatior");
+  if(n>slides.length) {slideIndex=1}
+  if(n<1) {slideInex = slides.length}
+  for (i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < indicatior.length; i++){
+    indicatior [i].className = indicator[i].className.replace("active","");
+  }
+  slides[slideIndex-1].style.display = "block";
+  indicator[slideIndex-1].className += "active";
+}
+
+$(".button").on("click", function (){
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal"|| className === "close"){
+    $(this).closest (".modal").hide();
+  }
+});
